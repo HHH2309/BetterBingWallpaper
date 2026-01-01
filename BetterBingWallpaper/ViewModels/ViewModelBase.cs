76be16668596c6e477b1008a833a8458f1b312aa
HHH2309 <yhwwdsj@163.com>
2026-01-01 21:08:40 +0800

    initialize commit

---
+++ BetterBingWallpaper/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BetterBingWallpaper.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ BetterBingWallpaper/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BetterBingWallpaper.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
